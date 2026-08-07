5d56dfcc182010b537fb0831fe8d87dba9ccffb8
Thunder <thunder@Mac.localdomain>
2026-08-07 13:15:21 -1000

    Stage 2: read endpoints with 404

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const tasks = [
+    {
+        id: 1,
+        title: "Learn Node.js",
+        done: true
+    },
+    {
+        id: 2,
+        title: "Build a REST API",
+        done: false
+    },
+    {
+        id: 3,
+        title: "Test the API",
+        done: false
+    }
+];
+
 app.get('/', (req, res) => {
     res.json({
         name: 'First Crud API',
@@ -15,7 +33,23 @@ app.get("/health", (req, res) => {
         status: "OK",
     });
 });
-    
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
+
+app.get('/tasks/:id', (req, res) => {  
+    const  taskId = parseInt(req.params.id);
+    const task = tasks.find(t => t.id === taskId);
+
+    if (!task) {
+        return res.status(404).json({ message: `Task ${taskId} not found` });
+    }
+
+    res.json(task);
+
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
