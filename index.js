109c3a34a7722c2cb3e10fabadc22466fe2f6d71
Thunder <thunder@Mac.localdomain>
2026-08-06 16:39:27 -1000

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('hello server');
+    res.json({
+        name: 'First Crud API',
+        version: '1.0',
+        endpoint: ["tasks"]
+    });
 });
 
+app.get("/health", (req, res) => {
+    res.json({
+        status: "OK",
+    });
+});
+    
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
