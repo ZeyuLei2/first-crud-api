fea61015949ebf63bed7f75e3af0f2f8dbcd1acd
Thunder <thunder@Mac.localdomain>
2026-08-10 18:40:36 -1000

    Stage 5: Swagger UI

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.json());
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./openapi.json");
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 const tasks = [
     {
